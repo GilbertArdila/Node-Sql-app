89d6f522c217c01b66d2823d1dcc6c49d3ceb4c6
Gilbert Ardila <gilbertferney@gmail.com>
2023-05-21 14:47:23 -0500

    Update config.js
    Adding variables manual

--- src/dataBase/config.js
+++ src/dataBase/config.js
@@ -2,8 +2,8 @@ const { config } = require('../config/config');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-
+//const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://postgres:AKF6UqYJgFkAj9BnDXnT@AKF6UqYJgFkAj9BnDXnT:7104/railway`;
 module.exports = {
   development: {
     url: URI,
