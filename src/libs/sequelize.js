f4a7eeb7ffb422e07dc7825c16a7b3f3735155cc
Gilbert Ardila <gilbertferney@gmail.com>
2023-05-21 14:47:42 -0500

    Update sequelize.js

--- src/libs/sequelize.js
+++ src/libs/sequelize.js
@@ -5,8 +5,8 @@ const setupModels = require('../dataBase/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-
+//const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://postgres:AKF6UqYJgFkAj9BnDXnT@AKF6UqYJgFkAj9BnDXnT:7104/railway`;
 const sequelize = new Sequelize(URI, {
   dialect: 'postgres',
   logging: true,
