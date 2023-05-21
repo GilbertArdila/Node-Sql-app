8be8db4dab146f017abb657b055ae1ccde1db0cd
Gilbert Ardila <gilbertferney@gmail.com>
2023-05-21 14:51:50 -0500

    Update config.js

--- src/dataBase/config.js
+++ src/dataBase/config.js
@@ -2,8 +2,7 @@ const { config } = require('../config/config');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-//const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-const URI = `postgres://postgres:AKF6UqYJgFkAj9BnDXnT@AKF6UqYJgFkAj9BnDXnT:7104/railway`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 module.exports = {
   development: {
     url: URI,
